0ec4eb117f9b54124d50d319b548654d90befe42
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-02 22:15:29 +0200

    rename file

---
+++ PythonRegex.py
@@ -0,0 +1,15 @@
+# Takes an email address as input and uses regular expressions 
+# to determine if it is a valid email address. 
+# Function should return True if email address is valid and False otherwise.
+def validateEmail(email):
+    
+    return False
+
+
+def main():
+    email = "dd"
+    print(validateEmail(email))
+
+
+if __name__ == '__main__':
+    main()
