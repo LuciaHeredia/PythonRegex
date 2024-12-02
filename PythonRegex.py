1b24cd1f4b22201f3b2c1dd5e3afe972d47f792a
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-03 01:18:44 +0200

    extractingHashtags done

--- PythonRegex.py
+++ PythonRegex.py
@@ -3,13 +3,13 @@ import re
 def validateEmail(email: str) -> bool:
     '''
     pattern:
-        ^ matches start of string.
-        [a-zA-Z0-9._%+-]+ matches one or more: alphanumeric characters, dots, underscores, percent signs, plus signs, or hyphens.
-        @ matches @ symbol.
-        [a-zA-Z0-9.-]+ matches one or more: alphanumeric characters, dots, or hyphens.
-        \. matches dot before the top-level domain.
-        [a-zA-Z]{2,} matches the top-level domain (must be at least 2 characters long).
-        $ matches end of string.
+        ^ -matches start of string.
+        [a-zA-Z0-9._%+-]+ -matches one or more: alphanumeric characters, dots, underscores, percent signs, plus signs, or hyphens.
+        @ -matches @ symbol.
+        [a-zA-Z0-9.-]+ -matches one or more: alphanumeric characters, dots, or hyphens.
+        \. -matches dot before the top-level domain.
+        [a-zA-Z]{2,} -matches the top-level domain (must be at least 2 characters long).
+        $ -matches end of string.
     '''
     pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     if re.match(pattern, email):
@@ -28,10 +28,10 @@ def checkValidateEmail():
 def extractPhoneNumbers(str_input: str) -> list[str]:
     '''
      pattern: (matches Isrsel Mobile numbers: 05XXXXXXXX)
-        05 phone start.
-        [0-9] one digit.
-        \-? optional - symbol.
-        [0-9]{7} other 7 digits.
+        05 -phone start.
+        [0-9] -one digit.
+        \-? -optional '-' symbol.
+        [0-9]{7} -other 7 digits.
     '''
     pattern = r"05[0-9]\-?[0-9]{7}"
     phone_numbers = re.findall(pattern, str_input)
@@ -80,7 +80,7 @@ def findingDuplicateWords(str_input: str) -> list[str]:
     '''
     pattern: 
         \b -Word boundary, ensures match is either at the start/end of a word.
-        (\w+) - Capturing group 1(a sequence of word characters).
+        (\w+) -Capturing group 1(a sequence of word characters).
         \w -matches any word character(alphanumeric plus underscore)
         + -quantifier indicates one or more occurrences.
         \b -Word boundary, ensuring captured group is a complete word.
@@ -97,8 +97,33 @@ def checkFindingDuplicateWords():
     print(findingDuplicateWords(str_input))
 
 
+def extractingHashtags(str_input: str) -> list[str]:
+    # hashtag - word/phrase starting with '#' symbol.
+    '''
+    pattern: 
+        # -matches '#' symbol.
+        \w -matches any word character, which includes letters, numbers, and underscores.
+        + -matches one or more of the preceding element(\w).
+    '''
+    pattern = r"#\w+"
+    return re.findall(pattern, str_input)
+
+
+def checkExtractingHashtags():
+    str_input = "# ##ew5 hello #exit" # example
+    print(extractingHashtags(str_input))
+
+
+'''
+Exercise 7: Extracting IP Addresses
+Write a Python function that takes a string as input and extracts all the IP addresses
+from it using regular expressions. The function should return a list of all the IP
+addresses found in the input string.
+'''
+
+
 def main():
-    checkFindingDuplicateWords()
+    checkExtractingHashtags()
 
 
 if __name__ == '__main__':
