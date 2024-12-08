f6c77d64c0f27417eaf9ebb5e669e227cea8e73e
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-08 17:23:37 +0200

    match_str_ax() done

--- PythonRegexKaggle.py
+++ PythonRegexKaggle.py
@@ -2,7 +2,7 @@ import re
 
 
 def is_str_alphanumeric(str_input: str):
-    #  check if string contains only a certain set of characters (in this case a-z, A-Z and 0-9).
+    #  1. check if string contains only a certain set of characters (in this case a-z, A-Z and 0-9).
     '''
      pattern: 
         \W -Matches any non-alphanumeric character, equivalent to [^a-zA-Z0-9_].
@@ -12,7 +12,6 @@ def is_str_alphanumeric(str_input: str):
         return True
     return False
 
-
 def check_is_str_alphanumeric():
     str_input = "dfgsd45345g5434234" # example
     if is_str_alphanumeric(str_input):
@@ -21,10 +20,28 @@ def check_is_str_alphanumeric():
         print(str_input," - Is non-alphanumeric.")
 
 
-def main():
-    check_is_str_alphanumeric()
-    #print('')
+def match_str_ax(str_input: str):
+    # 2. match a string that has an 'a' followed by none or more 'b's
+    '''
+     pattern: 
+        
+    '''
+    pattern = r"ab*?"
+    if re.search(pattern, str_input):
+        return True
+    return False
 
+def check_match_str_ax():
+    str_input = "abb" # example
+    if match_str_ax(str_input):
+        print("Found a match.")
+    else:
+        print("No match.")
+
+
+def main():
+    #check_is_str_alphanumeric()
+    check_match_str_ax()
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 [PythonRegexKaggle.py](https://github.com/LuciaHeredia/PythonRegex/blob/master/PythonRegexKaggle.py)
 ([Kaggle Source](https://www.kaggle.com/code/albeffe/regex-exercises-solutions/notebook))
 1. Is string alphanumeric
-2. ??
+2. Match string containing: 'a' followed by none or more 'b's
 3. ??
 4. ??
 5. ??
